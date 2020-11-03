5e80a951ec4d6852d05da3eb8bb0ac37e239cd3b
Robbie <robbruern@gmail.com>
2020-11-03 02:40:44 +0000

    New change

--- src/App.js
+++ src/App.js
@@ -2,9 +2,51 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Amplify, { API } from 'aws-amplify';
-import awsconfig from './aws-exports';
+import { Auth } from 'aws-amplify';
+import awsconfig from './aws-exports.js';
 
 Amplify.configure(awsconfig);
+
+async function signUp() {
+  try {
+      const { user } = await Auth.signUp({
+          username,
+          password,
+          attributes: {
+              email,          // optional
+              phone_number,   // optional - E.164 number convention
+              // other custom attributes 
+          }
+      });
+      console.log(user);
+  } catch (error) {
+      console.log('error signing up:', error);
+  }
+}
+
+async function confirmSignUp() {
+  try {
+    await Auth.confirmSignUp(username, code);
+  } catch (error) {
+      console.log('error confirming sign up', error);
+  }
+}
+
+async function signIn() {
+  try {
+      const user = await Auth.signIn(username, password);
+  } catch (error) {
+      console.log('error signing in', error);
+  }
+}
+
+async function signOut() {
+  try {
+      await Auth.signOut();
+  } catch (error) {
+      console.log('error signing out: ', error);
+  }
+}
 
 function App() {
   return (
