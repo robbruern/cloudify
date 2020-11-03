1472dfedbee78be9076d4afb714e9e7173310e62
Robbie <robbruern@gmail.com>
2020-11-03 02:18:40 +0000

    Api test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Amplify, { API } from 'aws-amplify';
+import awsconfig from './aws-exports';
+
+Amplify.configure(awsconfig);
 
 function App() {
   return (
