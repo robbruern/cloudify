f02d9751f8f054864f4cefa0b13989c99232f061
ricky <rmacha2@illinois.edu>
2020-11-04 22:47:31 -0600

    connector

--- backend/mysql.py
+++ backend/mysql.py
@@ -1,1 +1,14 @@
-ricky is gay
+import mysql.connector
+
+
+db = mysql.connector.connect(host="ec2-user@ip-172-31-17-246", user="ec2-user", passwd="", db="Music")
+cursor = self.db.cursor()
+
+add_log = ("INSERT INTO SpotifyArtist"
+"(ArtistID, SongID, ArtistName, Genre)"
+"VALUES (%s, %s, %s, %s)")
+
+data = ("asdgasdg", "asdfkjfasd", "Filip", "Sad")
+
+cursor.execute(add_log, data)
+db.commit()
